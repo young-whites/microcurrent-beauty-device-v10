dbfce059d67a1b5af16aaa0f35c17773862e2737
young-nights <2863692760@qq.com>
2026-07-09 10:40:15 +0800

    docs: sync pin mapping and protocol func codes across docs
    - 项目需求文档.md: fix heating control pins (PC10/PC11, not PC11/PC12)
    - 通讯协议文档.md: fix func code range to 0x01~0x09
    - 我的项目概要.md: add waveform switch command 0x09 to list

---
+++ workItem-djm-v10/applications/macBSP/Inc/bsp_hard.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2026-07-08     Administrator       the first version
+ */
+#ifndef APPLICATIONS_MACBSP_INC_BSP_HARD_H_
+#define APPLICATIONS_MACBSP_INC_BSP_HARD_H_
+
+
+
+#endif /* APPLICATIONS_MACBSP_INC_BSP_HARD_H_ */
---
+++ workItem-djm-v10/applications/macBSP/Src/bsp_hard.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2026-07-08     Administrator       the first version
+ */
+#include "bsp_hard.h"
+
+
+
+
+
+
+
+
+
