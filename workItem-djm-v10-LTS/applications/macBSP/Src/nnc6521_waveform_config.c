bb83528b540e6a224dd37aa3d34088e72e4b71fc
young-nights <2863692760@qq.com>
2026-07-21 16:51:38 +0800

    test(waveform): WF1 half_wave_clk=20000 re-verify

--- workItem-djm-v10-LTS/applications/macBSP/Src/nnc6521_waveform_config.c
+++ workItem-djm-v10-LTS/applications/macBSP/Src/nnc6521_waveform_config.c
@@ -35,7 +35,7 @@ const waveform_config_t g_waveform_configs[WAVEFORM_COUNT] =
         .waveform_type   = WAVEFORM_TYPE_SQUARE,     /* Square wave (edge-softened) */
         .gen_method      = GEN_METHOD_CUSTOM_SPI,      /* Custom SPI softened waveform */
         .point_num       = 64,       /* 64 points */
-        .half_wave_clk   = 25000,   /* TEST C: reverse-engineer timing */
+        .half_wave_clk   = 20000,   /* TEST: verify original value */
         .silent_time     = 300,     /* 150 * 2 = 300 */
         .rest_time       = 0,       /* No dead zone */
         .carrier_clk     = 0,       /* Not AM mode */
